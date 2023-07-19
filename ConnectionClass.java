3ad70c14cd6ceeda138ed613288aabb3136cf363
ronaksaini2000 <138023338+ronaksaini2000@users.noreply.github.com>
2023-07-20 02:17:46 +0530

    Add files via upload

---
+++ ConnectionClass.java
@@ -0,0 +1,25 @@
+package FeeReportManagement;
+import java.sql.*;
+
+public class ConnectionClass 
+{
+Connection con;
+Statement stm;
+ConnectionClass()
+{
+    try{     
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        con = DriverManager.getConnection("jdbc:mysql://localhost:3306/fee","root","root");
+        stm= con.createStatement();
+      
+        }
+    catch(Exception e)
+    {
+        e.printStackTrace(); 
+    }
+}
+public static void main(String args[])
+{
+    new ConnectionClass(); 
+}
+}
